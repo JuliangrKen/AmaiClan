d4473dcd2c3b582dd7acc273a44b58b7be1ebd90
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-22 19:32:26 +0300

    Добавил статический метод для получения DiscordBotConfig

--- AmaiClan.Bot/Configuration/DiscordBotConfig.cs
+++ AmaiClan.Bot/Configuration/DiscordBotConfig.cs
@@ -1,4 +1,7 @@
-﻿namespace AmaiClan.Bot.Configuration
+﻿using AmaiClan.Bot.Exceptions;
+using System.Text.Json;
+
+namespace AmaiClan.Bot.Configuration
 {
     /// <summary>
     /// Класс, инкапсулирующий значения из DiscordBotConfig.json
@@ -6,5 +9,12 @@
     public class DiscordBotConfig
     {
         public string? Token { get; set; }
+
+        public static DiscordBotConfig GetFromFile(string? path = null)
+        {
+            path ??= $@"{Environment.CurrentDirectory}\Configuration\DiscordBotConfig.json";
+            var json = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<DiscordBotConfig>(json) ?? throw new FailedDeserializeObjectException();
+        }
     }
 }
