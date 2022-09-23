de8a0b853c3d68d9e97931f933a8106ee0bb2036
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-23 09:54:55 +0300

    Добавил сервис логирования сообщений от приложения бота в консоль

---
+++ AmaiClan.Bot/Services/ConsoleLogger.cs
@@ -0,0 +1,13 @@
+﻿using Discord;
+
+namespace AmaiClan.Bot.Services
+{
+    public class ConsoleLogger : ILogger
+    {
+        public Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ AmaiClan.Bot/Services/ILogger.cs
@@ -0,0 +1,9 @@
+﻿using Discord;
+
+namespace AmaiClan.Bot.Services
+{
+    public interface ILogger
+    {
+        Task Log(LogMessage arg);
+    }
+}
