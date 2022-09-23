52c02cc722fcc62de9b342ade8d6bf0901f39006
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-23 12:52:20 +0300

    Добавил первую команду

---
+++ AmaiClan.Bot/Commands/Everyone/Roll.cs
@@ -0,0 +1,26 @@
+﻿using Discord;
+using Discord.Interactions;
+
+namespace AmaiClan.Bot.Commands.Everyone
+{
+    /// <summary>
+    /// Команда получения случайного числа
+    /// </summary>
+    public class Roll : SlashCommandBase
+    {
+        [SlashCommand("roll", "получить случайное число")]
+        public async Task Invoke(int max = 100, int min = 1)
+        {
+            (max, min) = max < min ? (min, max) : (max, min);
+
+            var result = new Random().Next(min, max);
+
+            var embedBuilder = new EmbedBuilder()
+                .WithTitle($"Результат: {result}")
+                .WithFooter($"Случайное число от {min} до {max}.")
+                .WithColor(GetRandomColor());
+
+            await RespondAsync(embed: embedBuilder.Build());
+        }
+    }
+}
---
+++ AmaiClan.Bot/Commands/SlashCommandBase.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using Discord.Interactions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AmaiClan.Bot.Commands
+{
+    public abstract class SlashCommandBase : InteractionModuleBase<SocketInteractionContext>
+    {
+        protected DiscordBot DiscordBot { get; set; }
+
+        protected SlashCommandBase()
+        {
+            DiscordBot = DiscordBot.Services.BuildServiceProvider().GetService<DiscordBot>() ?? 
+                throw new ArgumentNullException();
+        }
+
+        /// <summary>
+        /// Метод для получения случайного цвета
+        /// </summary>
+        /// <returns></returns>
+        protected Color GetRandomColor()
+        {
+            // Поставлен такой минимум, чтобы цвета не были тёмными
+            var min = 150;
+            var max = 255;
+
+            var random = new Random();
+            return new Color(random.Next(min, max), random.Next(min, max), random.Next(min, max));
+        }
+    }
+}
