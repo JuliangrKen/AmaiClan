94e4241b48e2294a7b2f49251213ae2b33a291a5
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-23 23:32:50 +0300

    Добавил команду для создания embed

---
+++ AmaiClan.Bot/Commands/Admin/Embed.cs
@@ -0,0 +1,33 @@
+﻿using Discord;
+using Discord.Interactions;
+
+namespace AmaiClan.Bot.Commands.Admin
+{
+    /// <summary>
+    /// Команда для создания Embed
+    /// </summary>
+    public class Embed : SlashCommandBase
+    {
+        [SlashCommand("embed", "создать embed")]
+        public async Task Invoke(
+            string? author = null,
+            string? title = null,
+            string? description = null,
+            string? footer = null,
+            string? imageUrl = null
+            )
+        {
+            var embedBuilder = new EmbedBuilder();
+
+            embedBuilder.WithAuthor(author);
+            embedBuilder.WithTitle(title);
+            embedBuilder.WithDescription(description);
+            embedBuilder.WithFooter(footer);
+            embedBuilder.WithImageUrl(imageUrl);
+            embedBuilder.WithColor(GetRandomColor());
+
+            //var a = new RequestOptions();
+            await RespondAsync(embed: embedBuilder.Build());
+        }
+    }
+}
