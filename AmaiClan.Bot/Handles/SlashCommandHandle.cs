99ad0e0c30ecbb1d86cc17c2e5dd68714e97461d
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-23 12:52:02 +0300

    Добавил обработчик / команд

---
+++ AmaiClan.Bot/Handles/SlashCommandHandle.cs
@@ -0,0 +1,43 @@
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace AmaiClan.Bot.Handles
+{
+    public class SlashCommandHandle
+    {
+        private readonly DiscordBot discordBot;
+        private readonly InteractionService interactionService;
+
+        public SlashCommandHandle(DiscordBot discordBot)
+        {
+            this.discordBot = discordBot;
+            interactionService = new InteractionService(discordBot.SocketClient);
+        }
+
+        public async Task HandleAsync()
+        {
+            var provider = DiscordBot.Services.BuildServiceProvider();
+            var client = discordBot.SocketClient;
+
+            await interactionService.AddModulesAsync(Assembly.GetEntryAssembly(), provider);
+            await RegisterCommandForEveryoneGuilds(client);
+
+            client.InteractionCreated += async interaction =>
+            {
+                var scope = provider.CreateScope();
+                var ctx = new SocketInteractionContext(client, interaction);
+                await interactionService.ExecuteCommandAsync(ctx, scope.ServiceProvider);
+            };
+        }
+
+        private Task RegisterCommandForEveryoneGuilds(DiscordSocketClient client)
+        {
+            foreach(var guild in client.Guilds)
+                interactionService.RegisterCommandsToGuildAsync(guild.Id);
+
+            return Task.CompletedTask;
+        }
+    }
+}
