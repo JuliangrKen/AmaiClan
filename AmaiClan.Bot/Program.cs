bd6c3b674fab1e1e9ee3c28ca53ef0bcb703878c
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-23 09:55:40 +0300

    Оформил входную точку приложения

--- AmaiClan.Bot/Program.cs
+++ AmaiClan.Bot/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AmaiClan.Bot;
+using AmaiClan.Bot.Configuration;
+using AmaiClan.Bot.Services;
+using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+// Конфигурация бота
+var socketConfig = new DiscordSocketConfig()
+{
+    AlwaysDownloadUsers = true,
+    ConnectionTimeout = 10_000,
+    MessageCacheSize = 100,
+    GatewayIntents = GatewayIntents.AllUnprivileged
+};
+
+// Инициализация, настойка и запуск бота
+var discordBot = new DiscordBot(socketConfig, DiscordBotConfig.GetFromFile());
+
+// Добавляем сервисы
+discordBot.Services.AddSingleton<ILogger, ConsoleLogger>();
+
+await discordBot.Run();
