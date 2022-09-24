694918c2ba5fdc7a643d240c35d4314057ea7493
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-24 11:13:01 +0300

    Добавил команду очистки сообщений

---
+++ AmaiClan.Bot/Commands/Admin/Clear.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace AmaiClan.Bot.Commands.Admin
+{
+    public class Clear : SlashCommandBase
+    {
+        [SlashCommand("clear", "очистить сообщения канала")]
+        public async Task Invoke(int num)
+        {
+            if (num == 0 || num > 100 || num < 0)
+            {
+                await RespondAsync("Некорректный ввод!");
+                return;
+            }
+
+            await RespondAsync($"Задача принята, {num} сообщений будет удалены!");
+
+            Thread.Sleep(1000);
+
+            num++; // + сообщение, отправленное самим ботом
+
+            var messages = await Context.Channel.GetMessagesAsync(num).FlattenAsync();
+
+            var channel = Context.Channel as SocketTextChannel ?? throw new ArgumentException();
+            await channel.DeleteMessagesAsync(messages);
+        }
+    }
+}
