f4a75a209cd8835fe0b1403e4e82f951993bd4ea
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-24 10:19:12 +0300

    Добавил /avatar

---
+++ AmaiClan.Bot/Commands/Everyone/Avatar.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Discord.Interactions;
+
+namespace AmaiClan.Bot.Commands.Everyone
+{
+    public class Avatar : SlashCommandBase
+    {
+        [SlashCommand("avatar", "получить случайное число")]
+        public async Task Invoke(IUser user)
+        {
+            var avatarUrl = user.GetAvatarUrl(ImageFormat.Auto, 2048);
+
+            var embedBuilder = new EmbedBuilder()
+                .WithTitle(user.Username)
+                .WithUrl(avatarUrl)
+                .WithImageUrl(avatarUrl)
+                .WithColor(GetRandomColor());
+
+            await RespondAsync(embed: embedBuilder.Build());
+        }
+    } 
+}
