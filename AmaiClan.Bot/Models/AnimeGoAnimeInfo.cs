3a26b33389555b8d5499cb612a91614a9852d5fd
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-10-01 23:08:58 +0300

    Добавил модель данных с сайта https://animego.org/

---
+++ AmaiClan.Bot/Models/AnimeGoAnimeInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace AmaiClan.Bot.Models
+{
+    /// <summary>
+    /// Модель данных со страницы аниме из https://animego.org/
+    /// </summary>
+    public class AnimeGoAnimeInfo
+    {
+        /// <summary>
+        /// Название аниме
+        /// </summary>
+        public string? Name { get; set; }
+        /// <summary>
+        /// Url на страницу сайта
+        /// </summary>
+        public string? SiteUrl { get; set; }
+        /// <summary>
+        /// Url на постер 
+        /// </summary>
+        public string? ImageUrl { get; set; }
+    }
+}
