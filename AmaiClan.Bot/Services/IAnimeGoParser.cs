a26ed44a9fa8b887daf326b12b4f6479db418b00
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-10-01 23:12:06 +0300

    Добавил интерфейс

---
+++ AmaiClan.Bot/Services/IAnimeGoParser.cs
@@ -0,0 +1,18 @@
+﻿using AmaiClan.Bot.Models;
+
+namespace AmaiClan.Bot.Services
+{
+    /// <summary>
+    /// Интерфейс для парсинга сайта https://animego.org/
+    /// </summary>
+    public interface IAnimeGoParser
+    {
+        const string SiteUrl = "https://animego.org/";
+
+        /// <summary>
+        /// Метод для получения данных со случайной страницы аниме сайта
+        /// </summary>
+        /// <returns>Модель, инкапсулирующая данные об аниме</returns>
+        Task<AnimeGoAnimeInfo> GetRandomAnimeGoAnimeInfoAsync();
+    }
+}
