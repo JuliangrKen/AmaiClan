867094a3dfbfb7e2d4f3c0d3b114969ee3711c36
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-10-01 17:35:50 +0300

    Добавил сервис для получения случайных 18+ артов

---
+++ AmaiClan.Bot/Services/IRule34Parser.cs
@@ -0,0 +1,18 @@
+﻿namespace AmaiClan.Bot.Services
+{
+    /// <summary>
+    /// Интерфейс для парсинга сайта https://rule34.xyz/
+    /// </summary>
+    public interface IRule34Parser
+    {
+        /// <summary>
+        /// Ссылка на сайт, подвергающийся парсингу
+        /// </summary>
+        const string SiteUrl = "https://rule34.xyz/";
+        /// <summary>
+        /// Метод для получения ссылки на существующее случайное изображение
+        /// </summary>
+        /// <returns>Url изображение</returns>
+        Task<string> GetRandomImageUrlAsync();
+    }
+}
---
+++ AmaiClan.Bot/Services/Rule34Parser.cs
@@ -0,0 +1,70 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AmaiClan.Bot.Services
+{
+    /// <summary>
+    /// Класс, реализующий интерфейс для парсинга сайта https://rule34.xyz/
+    /// </summary>
+    public class Rule34Parser : IRule34Parser
+    {
+        private readonly HttpClient httpClient;
+        private readonly ILogger logger;
+        private readonly Random random;
+
+        private readonly int MinIdImage;
+        private readonly int MaxIdImage;
+
+        private readonly string httpParsePattern;
+
+        public Rule34Parser(HttpClient httpClient, ILogger logger)
+        {
+            this.httpClient = httpClient;
+            this.logger = logger;
+            random = new Random();
+
+            MinIdImage = 3_500_000;
+            MaxIdImage = 3_505_000;
+
+            httpParsePattern = "class=\"img shadow-base\" src=\"(.+?)\"";
+        }
+
+        public async Task<string> GetRandomImageUrlAsync()
+        {
+            var postId = random.Next(MinIdImage, MaxIdImage);
+
+            while (true)
+            {
+                var url = $"{IRule34Parser.SiteUrl}post/{postId}";
+
+                var respond = await httpClient.GetAsync(url);
+                await logger.Log($"Совершена попытка подключения к {url}");
+
+                if (respond.IsSuccessStatusCode)// пост не найдет проверка
+                {
+                    await logger.Log($"Подключение к {url} произведено успешно!");
+                    return await GenerateCorrectImageUrlAsync(await respond.Content.ReadAsStringAsync());
+                }
+
+                await logger.Log($"Подключиться к {url} не удалось!");
+                postId = postId <= MaxIdImage ? postId++ : throw new Exception();
+            }
+        }
+        
+        private async Task<string> GenerateCorrectImageUrlAsync(string httpContent)
+        {
+            var match = Regex.Match(httpContent, httpParsePattern);
+
+            if (!match.Success)
+                throw new ArgumentException(nameof(match.Success));
+
+            var imageUrl = match.Groups[1].Value;
+
+            if (!imageUrl.Contains("https://rule34.xyz/"))
+                imageUrl = "https://rule34.xyz" + imageUrl;
+
+            await logger.Log($"{imageUrl} - {nameof(imageUrl)}");
+
+            return imageUrl;
+        }
+    }
+}
